943f1e919b47fb62d7f2e397c5a66d88c7ac5802
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-22 22:40:41 +0530

    final push

--- Lab2/obj3.asm
+++ Lab2/obj3.asm
@@ -7,18 +7,19 @@
 ; indirect addressing mode.
 
 ; i. Swapping of nibble of data1
-MOV AX, 0000H
-MOV DS, AX;     Initialize data segment
 MOV SI, 0500H;  Set offset value
 MOV AL, [SI];   Data1 = 12
 INC SI
 MOV AH, [SI];   Data2 = 34
+INC SI
+
 ROR AL, 04H;    Rotate by 4 locations
 ROR AH, 04H;    Rotate by 4 locations
-INC SI; 
+
 MOV [SI], AL;   Move value of AL to physical memory
 INC SI
 MOV [SI], AH;   Move value of AH to physical memory
+INC SI
 
 ; ii. Y = (data1 and data2) or (data1 xor data2);
 MOV BL, AL;     Copy AL value to BL (data1)
@@ -26,6 +27,6 @@ MOV BH, AH;     Copy AH value to BH (data2)
 AND BH, BL;     Apply AND operation on data1 and data2, result stored in BH
 XOR AH, AL;     Apply XOR operation on data1 and data2, result stored in AH
 OR AH, BH;      Apply OR operation on result
-INC SI
+
 MOV [SI], AH;   Move contents of result to memory
 HLT
--- Lab5_6/lab5_4.s
+++ Lab5_6/lab5_4.s
@@ -5,14 +5,14 @@
 .global _start
 _start:
 
-	ldr r0, =a		// 1st memory location
-	ldr r1, =b		// 2nd memory location 
-	ldr r2, [r0]	// load first number to r2
-	ldr r3, [r1]	// load 2nd numbe to r3
+	ldr r0, =a			// 1st memory location
+	ldr r1, =b			// 2nd memory location 
+	ldr r2, [r0]		// load first number to r2
+	ldr r3, [r1]		// load 2nd numbe to r3
 	and r4, r2, r3	// and operation r3, r2 and store to r4
 	orr r5, r2, r3	// or operation r3, r2 and store to r4
 	eor r6, r2, r3	// xor operation r3, r2 and store to r4
-	mvn r7, r2	// not operation r2 and store to r4
+	mvn r7, r2			// not operation r2 and store to r4
 	
 
 	.data
