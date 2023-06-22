0f7ab43f61bebadf041067880c2bec09f7e960be
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-22 21:40:39 +0530

    updated code for lab3, obj1

--- Lab3/obj1.asm
+++ Lab3/obj1.asm
@@ -4,21 +4,23 @@
 ; Section: K
 ; LAB-1: Find the sum and average of N 16-bit numbers
 
-MOV SI, 2000H   ; Initialize stack index to 2000h
-MOV CL, [SI]    ; Give offset address as 2000h for giving value of n (8 BIT number)
-MOV BL, CL      ; Save the copy of n in bx BL<=CL
-MOV DX, 0000H   ; Initialize DX to 0000h for storing overflow
+MOV SI, 2000H               ; Initialize stack index to 2000h
+MOV CL, [SI]                ; Give offset address as 2000h for giving value of n (8 BIT number)
+MOV BL, CL                  ; Store the copy of address 
 INC SI
-L1:      
-ADD AX, [SI]    ; Add each number a1, a2,..., an to AX
-INC SI          ; Increment stack index by 16bit to get the next number ai
-INC SI
-JNC L2          ; If carry flag is zero i.e. no overflow Jump to L2 else increment DX for overflow case.
-INC DX
-L2: DEC CL      ; keep decreasing CL i.e. numbers to read
-JNZ L1          ; If CL is not zero jump to L1 else move to next instruction
-MOV [SI], AX    ; Store/move value of sum of next 16 bit address
-MOV [SI+2], DX  ; Store the Most significant bits (Overflow values for >16bit) in next 16bit location
-DIV BX          ; Divide AX<=AX/BX to get Average
-MOV [SI+4], AX  ; Store avg.
+MOV AX, 0000H       
+
+FIND_SUM: ADD AX, [SI]      ; Add values
+          JNC NEXT          ; Go to next element if no overflow
+            
+          INC DX            ; Overflow condition.
+            
+NEXT:     INC SI          
+          INC SI            ; Increment twice for next memory location (16 bits)
+          LOOP FIND_SUM     ; Loop auto-decrements CX/CL
+
+MOV [SI], AX                ; Store sum in next memory location
+MOV [SI+2], DX              ; Store the Most significant bits (Overflow values for >16bit) in next 16bit location
+DIV BX                      ; Divide AX<=AX/BX to get Average
+MOV [SI+4], AX              ; Store average.
 HLT
