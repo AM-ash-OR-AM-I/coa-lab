f90a484028faa12b5d6e3a65b0658015ef45e893
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-22 20:37:32 +0530

    updated code for obj-3

--- Lab3/obj3.asm
+++ Lab3/obj3.asm
@@ -3,14 +3,14 @@
 ; Regd. No: 2141013014
 ; Section: K
 ; LAB-3: Move the 16 bits from one location to another
+MOV SI,1000H    ; 1st memory location
+MOV DI,2000H    ; 2nd memory location
+MOV CL,[SI]     ; Length of array
+INC SI          ; Check for next element          
 
-MOV SI, 2000H
-MOV CL, [SI]
-INC SI
-L1: MOV AX, [SI]
-MOV [SI+100], AX
-INC SI
-INC SI
-DEC CL
-JNZ L1
-HLT
+ITERATE:MOV AX,[SI] ; Move element to temporary register AX 
+        MOV [DI],AX ; Store element in 2nd memory location
+        INC SI      ; Go to next memory location to scan
+        INC DI      ; Move pointer to next memory location to move
+        LOOP ITERATE; Iterate untill all elements scanned
+HLT 
